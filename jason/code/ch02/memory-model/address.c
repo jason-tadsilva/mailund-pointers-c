64d10ffdd902145ab90d32e4e8cf0c0d228dcf60
Jason <jason.tad@icloud.com>
2024-04-29 12:57:40 +0900

    Update jason/code

--- jason/code/ch02/memory-model/address.c
+++ jason/code/ch02/memory-model/address.c
@@ -13,8 +13,8 @@ int main(void)
 
     // to print the result returned by sizeof
     // use either %lu or %zu format specifier
-    printf("sizeof(char) = %zu\n", sizeof(char));
-    printf("sizeof(char*) = %lu\n", sizeof(char*));
+    printf("sizeof(char) = %zu byte\n", sizeof(char));
+    printf("sizeof(char*) = %lu bytes\n", sizeof(char*));
 
     return 0;
 }
